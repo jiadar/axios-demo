eb809aa9521e76b7fd7dffd4b220c9e859c6a5e8
Ross Capdeville <ross@teamengine.org>
2017-08-03 14:22:48 -0700

    update

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ function performGetRequest1() {
 
   resultElement.innerHTML = '';
   
-  axios.post('http://jsonstub.com/users',
-            { headers: {
-              "JsonStub-User-Key": "605f7a33-036b-4d09-af63-91e5f103ebaf",
-              "JsonStub-Project-Key": "6b96b772-8e99-402a-9108-7964013effab"
-            }
-            })
+  axios.post('http://jsonstub.com/users')
   .then(function (response) {
     resultElement.innerHTML = generateSuccessHTMLOutput(response);
   })
