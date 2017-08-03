cc85cf64bfd20ba31e7b9819f6f9ec23482831a3
Ross Capdeville <ross@teamengine.org>
2017-08-03 14:24:20 -0700

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ function performGetRequest1() {
 
   resultElement.innerHTML = '';
   
-  axios.post('http://jsonstub.com/users')
+  axios({
+    url: "http://jsonstub.com/users",
+    headers: {"JsonStub-User-Key" : "605f7a33-036b-4d09-af63-91e5f103ebaf", 
+	      "JsonStub-Project-Key" : "6b96b772-8e99-402a-9108-7964013effab"},
+    timeout: 20000,
+    method: 'post',
+    responseType: 'json'
+  })
   .then(function (response) {
     resultElement.innerHTML = generateSuccessHTMLOutput(response);
   })
