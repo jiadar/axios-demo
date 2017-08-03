5edb2dddda29981a405ee1211f37adce175b9033
Ross Capdeville <ross@teamengine.org>
2017-08-03 14:56:32 -0700

    update

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+  const path = require('./');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
