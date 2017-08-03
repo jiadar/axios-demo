b2be2112e39ee63e714b67c38b1905095870db7f
Ross Capdeville <ross@teamengine.org>
2017-08-03 15:20:51 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './public/index.html'
   },
   devtool: 'inline-source-map',
   devServer: {
