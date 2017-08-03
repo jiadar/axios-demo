1d752be657eb472cfdd480c5ac48dec93bc365ed
Ross Capdeville <ross@teamengine.org>
2017-08-03 14:32:35 -0700

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -951,6 +951,7 @@ function performGetRequest1() {
   __WEBPACK_IMPORTED_MODULE_0_axios___default()("http://jsonstub.com/users", {
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
       "JsonStub-User-Key": "605f7a33-036b-4d09-af63-91e5f103ebaf", 
       "JsonStub-Project-Key": "6b96b772-8e99-402a-9108-7964013effab"},
     timeout: 20000,
