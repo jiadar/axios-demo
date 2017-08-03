3e00814880f042e1d942c6b8ea4697e5550c052e
Ross Capdeville <ross@teamengine.org>
2017-08-03 14:25:32 -0700

    update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ function performGetRequest1() {
   resultElement.innerHTML = '';
   
   axios("http://jsonstub.com/users", {
-    headers: {"JsonStub-User-Key" : "605f7a33-036b-4d09-af63-91e5f103ebaf", 
-	      "JsonStub-Project-Key" : "6b96b772-8e99-402a-9108-7964013effab"},
+    headers: {
+      "Content-Type: application/json",
+      "JsonStub-User-Key" : "605f7a33-036b-4d09-af63-91e5f103ebaf", 
+      "JsonStub-Project-Key" : "6b96b772-8e99-402a-9108-7964013effab"},
     timeout: 20000,
     method: 'post',
     responseType: 'json'
